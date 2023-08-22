a2931a94c869fbd785f3b1344652b80ca7477f24
xxo <xxoxec@gmail.com>
2023-08-22 23:51:50 +0900

    feat : first commit

---
+++ src/test/java/jpa/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
